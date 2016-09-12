0a6831d44f2c9dfecaccfd79f028f922c42d2e4f
Bahadır Özdemir <bahadirozdemir@Bahadrs-MacBook-Pro.local>
2016-09-12 04:49:39 +0300

    Queue.Services

---
+++ Controllers/SendQueueController.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace queue.Controllers
+{
+    [Route("api/[controller]")]
+    public class SendQueueController : Controller
+    {
+        [HttpPost]
+        public void SendQueue([FromBody]string body){
+            
+        }
+    }
+}
